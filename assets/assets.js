07373f3ffa1cd3391c5ec0724b14b08b8958ccb2
yasara-widyarathna <113191376+yasara-widyarathna@users.noreply.github.com>
2024-11-28 23:41:59 +0530

    Add files via upload

---
+++ assets/assets.js
@@ -0,0 +1,350 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Salad",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Rolls",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Deserts",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Sandwich",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Cake",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Pure Veg",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Pasta",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Noodles",
+        menu_image: menu_8
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Greek salad",
+        image: food_1,
+        price: 3360,
+        description: "Greek salad is a refreshing mix of cucumbers, tomatoes, olives, onions, and feta cheese, dressed with olive oil and lemon.",
+        category: "Salad"
+    },
+    {
+        _id: "2",
+        name: "Veg salad",
+        image: food_2,
+        price: 5040,
+        description: "A veg salad is a mix of fresh vegetables, topped with herbs and a light dressing, offering a healthy and refreshing meal.",
+        category: "Salad"
+    }, {
+        _id: "3",
+        name: "Clover Salad",
+        image: food_3,
+        price: 4480,
+        description: "Clover salad is a healthy mix of clover sprouts and vegetables, dressed with lemon juice or olive oil.",
+        category: "Salad"
+    }, {
+        _id: "4",
+        name: "Chicken Salad",
+        image: food_4,
+        price: 6720,
+        description: "Chicken salad is a flavorful mix of cooked chicken, vegetables, and creamy or tangy dressing.",
+        category: "Salad"
+    }, {
+        _id: "5",
+        name: "Lasagna Rolls",
+        image: food_5,
+        price: 3920,
+        description: "Lasagna rolls are noodles filled with cheese, sauce, and meat or vegetables, rolled up and baked.",
+        category: "Rolls"
+    }, {
+        _id: "6",
+        name: "Peri Peri Rolls",
+        image: food_6,
+        price: 3360,
+        description: "Peri Peri rolls are spicy wraps with grilled peri-peri chicken, vegetables, and sauces, wrapped in soft bread or tortillas.",
+        category: "Rolls"
+    }, {
+        _id: "7",
+        name: "Chicken Rolls",
+        image: food_7,
+        price: 5600,
+        description: "Chicken rolls are delicious wraps made with spiced chicken, vegetables, and sauces, rolled in a soft flatbread or tortilla.",
+        category: "Rolls"
+    }, {
+        _id: "8",
+        name: "Veg Rolls",
+        image: food_8,
+        price: 4200,
+        description: "Veg rolls are tasty wraps filled with spiced vegetables, herbs, and sauces, rolled in soft flatbread or tortillas.",
+        category: "Rolls"
+    }, {
+        _id: "9",
+        name: "Ripple Ice Cream",
+        image: food_9,
+        price: 3920,
+        description: "Ripple ice cream is a creamy dessert with swirls of flavored syrup, like chocolate or fruit, creating a marbled effect throughout.",
+        category: "Deserts"
+    }, {
+        _id: "10",
+        name: "Fruit Ice Cream",
+        image: food_10,
+        price: 6160,
+        description: "Fruit ice cream is a creamy dessert blended with fresh fruits or fruit flavors, offering a refreshing and naturally sweet treat.",
+        category: "Deserts"
+    }, {
+        _id: "11",
+        name: "Jar Ice Cream",
+        image: food_11,
+        price: 2800,
+        description: "Jar ice cream is a creative dessert served in layers with toppings, sauces, and flavors in a jar.",
+        category: "Deserts"
+    }, {
+        _id: "12",
+        name: "Vanilla Ice Cream",
+        image: food_12,
+        price: 3360,
+        description: "Vanilla ice cream is a classic, creamy dessert made with vanilla flavoring, offering a smooth and sweet treat.",
+        category: "Deserts"
+    },
+    {
+        _id: "13",
+        name: "Chicken Sandwich",
+        image: food_13,
+        price: 3360,
+        description: "A chicken sandwich is a bun filled with a grilled or fried chicken fillet and toppings like lettuce and tomato.",
+        category: "Sandwich"
+    },
+    {
+        _id: "14",
+        name: "Vegan Sandwich",
+        image: food_14,
+        price: 5040,
+        description: "A vegan sandwich is made with plant-based ingredients like vegetables, hummus, and vegan cheese, served in bread.",
+        category: "Sandwich"
+    }, {
+        _id: "15",
+        name: "Grilled Sandwich",
+        image: food_15,
+        price: 4480,
+        description: "A grilled sandwich is made by toasting bread with fillings like cheese, vegetables, or meats, until golden and crispy.",
+        category: "Sandwich"
+    }, {
+        _id: "16",
+        name: "Bread Sandwich",
+        image: food_16,
+        price: 6720,
+        description: "A bread sandwich is a simple sandwich made with slices of bread and various fillings like vegetables, spreads, or meats.",
+        category: "Sandwich"
+    }, {
+        _id: "17",
+        name: "Cup Cake",
+        image: food_17,
+        price: 3920,
+        description: "A cupcake is a small, individual-sized cake, often topped with frosting and decorations, perfect for a sweet treat.",
+        category: "Cake"
+    }, {
+        _id: "18",
+        name: "Vegan Cake",
+        image: food_18,
+        price: 3360,
+        description: "A vegan cake is a plant-based dessert made without dairy or eggs, using ingredients like flour, sugar, and plant-based milk.",
+        category: "Cake"
+    }, {
+        _id: "19",
+        name: "Butterscotch Cake",
+        image: food_19,
+        price: 5600,
+        description: "Butterscotch cake is a sweet, moist cake flavored with butterscotch, often topped with a rich butterscotch frosting or syrup.",
+        category: "Cake"
+    }, {
+        _id: "20",
+        name: "Sliced Cake",
+        image: food_20,
+        price: 4200,
+        description: "Sliced cake is a cake cut into pieces for easy serving, available in various flavors like chocolate, vanilla, or fruit.",
+        category: "Cake"
+    }, {
+        _id: "21",
+        name: "Garlic Mushroom ",
+        image: food_21,
+        price: 3920,
+        description: "Garlic mushrooms are mushrooms cooked with garlic, butter, and herbs, creating a flavorful and savory dish.",
+        category: "Pure Veg"
+    }, {
+        _id: "22",
+        name: "Fried Cauliflower",
+        image: food_22,
+        price: 6160,
+        description: "Fried cauliflower is battered or breaded cauliflower, deep-fried until crispy and golden.",
+        category: "Pure Veg"
+    }, {
+        _id: "23",
+        name: "Mix Veg Pulao",
+        image: food_23,
+        price: 2800,
+        description: "Mix Veg Pulao is a fragrant rice dish cooked with mixed vegetables and spices, offering a flavorful and colorful meal.",
+        category: "Pure Veg"
+    }, {
+        _id: "24",
+        name: "Rice Zucchini",
+        image: food_24,
+        price: 3360,
+        description: "Rice zucchini is a dish where zucchini is cooked with rice, often flavored with herbs and spices for a light, savory meal.",
+        category: "Pure Veg"
+    },
+    {
+        _id: "25",
+        name: "Cheese Pasta",
+        image: food_25,
+        price: 3360,
+        description: "Cheese pasta is a flavorful dish made with pasta, melted cheese, and a creamy sauce, offering a rich and satisfying meal.",
+        category: "Pasta"
+    },
+    {
+        _id: "26",
+        name: "Tomato Pasta",
+        image: food_26,
+        price: 5040,
+        description: "Tomato pasta is a dish made with pasta and a tomato-based sauce, flavored with garlic, herbs, and olive oil.",
+        category: "Pasta"
+    }, {
+        _id: "27",
+        name: "Creamy Pasta",
+        image: food_27,
+        price: 4480,
+        description: "Creamy pasta is a rich dish made with pasta and a smooth, creamy sauce, usually made from cream, cheese, and butter.",
+        category: "Pasta"
+    }, {
+        _id: "28",
+        name: "Chicken Pasta",
+        image: food_28,
+        price: 6720,
+        description: "Chicken pasta is a tasty dish made with pasta and chicken, usually served with a flavorful sauce like Alfredo or marinara.",
+        category: "Pasta"
+    }, {
+        _id: "29",
+        name: "Buttter Noodles",
+        image: food_29,
+        price: 3920,
+        description: "Butter noodles are pasta tossed in melted butter and seasoned with salt, pepper, and herbs for a simple, flavorful dish.",
+        category: "Noodles"
+    }, {
+        _id: "30",
+        name: "Veg Noodles",
+        image: food_30,
+        price: 3360,
+        description: "Veg noodles are stir-fried noodles mixed with vegetables and seasoned with soy sauce and spices for a tasty dish.",
+        category: "Noodles"
+    }, {
+        _id: "31",
+        name: "Somen Noodles",
+        image: food_31,
+        price: 5600,
+        description: "Somen noodles are thin Japanese wheat noodles, often served chilled with dipping sauce or in a warm broth.",
+        category: "Noodles"
+    }, {
+        _id: "32",
+        name: "Cooked Noodles",
+        image: food_32,
+        price: 4200,
+        description: "Cooked noodles are boiled or steamed noodles, ready to be paired with sauces, vegetables, or proteins for a complete meal.",
+        category: "Noodles"
+    }
+]
