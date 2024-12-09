380a3bbcd209b8f7621491c9696051776e3ace56
yasara-widyarathna <113191376+yasara-widyarathna@users.noreply.github.com>
2024-12-09 12:10:35 +0530

    Add files via upload

---
+++ assets.js
@@ -0,0 +1,17 @@
+
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+import profile_icon from './profile_icon.png'
+
+export const assets ={
+    
+    add_icon,
+    order_icon,
+    upload_area,
+    parcel_icon,
+    profile_icon
+}
+
+export const url = 'http://localhost:4000'
